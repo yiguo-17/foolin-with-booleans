8f061e7ffdbf7ffdddb9b06b4fe203ce9f53f985
Colin Jaffe <balloonasaurus@gmail.com>
2020-06-11 08:29:51 -0400

    Creates stronger logical scaffold for itsComplicated test cases.

--- main.test.js
+++ main.test.js
@@ -92,16 +92,16 @@ describe('either', () => {
   it('is a function', () => {
     expect(typeof either).toBe('function')
   })
-  
+
   it('returns `true` if both parameters are `true`', () => {
     expect(either(true, true)).toBe(true)
   })
-  
+
   it('returns `true` if either parameter is `true`', () => {
     expect(either(true, false)).toBe(true)
     expect(either(false, true)).toBe(true)
   })
-  
+
   it('returns `false` if both parameters are `false`', () => {
     expect(either(false, false)).toBe(false)
   })
@@ -165,7 +165,7 @@ describe('neither', () => {
     expect(neither(false, true)).toBe(false)
     expect(neither(true, false)).toBe(false)
   })
-  
+
   it(`returns false if both parmaeters are true`, () => {
     expect(neither(true, true)).toBe(false)
   })
@@ -191,9 +191,6 @@ describe('itsComplicated', () => {
   it('returns `false` if the first parameter is `true` and either the second or third parameter are `false`', () => {
     expect(itsComplicated(true, false, true)).toBe(false)
     expect(itsComplicated(true, true, false)).toBe(false)
-  })
-
-  it('returns `true` if the first parameter is `false` and both the second and third parameter are `true`', () => {
-    expect(itsComplicated(false, true, true)).toBe(true)
+    expect(itsComplicated(true, false, false)).toBe(false)
   })
 })
